a9b6ef7ea259cf835d79c601cee5193fefd237a6
Aliaksandr Iluishenka <sasha29292931@gmail.com>
2023-09-06 22:58:39 +0300

    feat: reworked array and create elements through objects

--- main.js
+++ main.js
@@ -2,25 +2,47 @@ const wrapper = document.querySelector('.wrapper');
 
 class Pairs {
   constructor() {
-    this.arr = [1, 1, 2, 2, 3, 3, 4, 4];
+    this.arr = [];
+  }
+  createItem(keys) {
+    for (let n = 0, i = 0; i < keys; i++) {
+      if (i % 2 === 0) {
+        n++;
+      }
+      const key = {
+        label: n,
+        id: i,
+        isOpen: false
+      }
+      this.arr.push(key);
+    }
   }
   renderItems() {
-    this.arr.forEach(num => this.createItem(num));
+    this.shuffle();
+    this.arr.forEach(el => this.createCard(el));
   }
-  createItem(num) {
+  createCard({ label, id, isOpen }) {
     const rectangle = document.createElement('div');
     rectangle.classList.add('rectangle');
+    rectangle.setAttribute("id", id);
     const number = document.createElement('span');
     number.classList.add('number');
-    number.innerText = num;
+    number.innerText = label;
     rectangle.append(number);
     wrapper.append(rectangle);
   }
   newGame() {
     this.renderItems();
   }
+  shuffle() {
+    for (let i = this.arr.length - 1; i > 0; i--) {
+      let m = Math.floor(Math.random() * (i + 1));
+      [this.arr[i], this.arr[m]] = [this.arr[m], this.arr[i]];
+    }
+  }
 }
 
 
 const game = new Pairs();
+game.createItem(8);
 game.newGame();
